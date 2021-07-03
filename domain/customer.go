4258ee1ea362222244b88bf55b9d9e7a043e6f17
Bruno Farias <brunofarias1@gmail.com>
2021-07-03 16:16:35 +0300

    fetching all users by status

--- domain/customer.go
+++ domain/customer.go
@@ -12,6 +12,7 @@ type Customer struct {
 }
 
 type CustomerRepository interface {
-	FindAll() ([]Customer, *errs.AppError)
+	// FindAll status == 1 status == 0 status == "
+	FindAll(status string) ([]Customer, *errs.AppError)
 	ById(string) (*Customer, *errs.AppError)
 }
